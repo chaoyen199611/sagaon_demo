60b0bda511478c41d244c520bb7f0f64f661a703
Chao-Yen Huang <chaoyen510220@gmail.com>
2022-09-09 18:55:20 +0800

    feat: add static file
    the static file includes css and js files, add a nav bar with hover effect

--- demo/views.py
+++ demo/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from django.views.generic.base import TemplateView
+
+
+class DemoPageView(TemplateView):
+
+    template_name="demo.html"
+
+    def get_context_data(self, *args,**kwargs):
+        context=super().get_context_data(*args,**kwargs)
+        return context
