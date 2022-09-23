012dbeb031f84dd43372047b77ded3a3ecc24128
Chao-Yen Huang <chaoyen510220@gmail.com>
2022-09-23 14:25:30 +0800

    style: typo

--- demo/views.py
+++ demo/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
-
+from django.http import HttpResponseBadRequest, JsonResponse
+from django.core import serializers
 from django.views.generic.base import TemplateView
 from . models import StationInfo,Weather,TripData
 
@@ -10,7 +10,18 @@ class DemoPageView(TemplateView):
 
     def get_context_data(self, *args,**kwargs):
         context=super().get_context_data(*args,**kwargs)
-        context['stations']=StationInfo.objects.all()
+        
         context['weathers']=Weather.objects.all()
-        context['tripdatas']=TripData.objects.all()
+        context['tripdatas']=TripData.objects.all().filter(time='2020-08-20 01:30:00').order_by('station_id')
+        context['stations']=StationInfo.objects.all()
+
+        # date=self.request.GET.get('key1')
+        # print(date)
         return context
+
+    # def get(self, *args, **kwargs):
+    #     date=self.request.GET.get('key1')
+    #     print(date)
+    #     trip = TripData.objects.all().filter(time=date).order_by('station_id')
+    #     qs_json = serializers.serialize('json', trip)
+    #     return JsonResponse(qs_json,safe=False)
